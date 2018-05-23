07857fc6fa2602b3f7cbb5a80eefc30bcdb77e27
vishalgupta97 <vishalgupta7972@gmail.com>
2018-05-23 15:51:35 +0530

    Serving static content and hot loading in flask file. Compiling with gcc in restart_test

--- docker-session/flask/app.py
+++ docker-session/flask/app.py
@@ -1,11 +1,24 @@
-from flask import Flask, request  # , current_app, jsonify
+from flask import Flask, request, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='')
 
 @app.route('/test/<name>')
 def test(name):
     return "It works, "+name+"!"
 
+@app.route('/',methods=['GET','POST'])
+def test1():
+	if request.method=='GET':
+		return send_from_directory('','index.html')
+	elif request.method=='POST':
+		expr=request.form.get('expr')
+		result=eval(expr)
+		return 'result: %s' % result 
+	
+@app.route('/hello')
+def test2():
+    return "Hello World1!"
+
 if __name__ == "__main__":
     # app.run(debug=True, threaded=True)
-    app.run(threaded=True, host="0.0.0.0", port=5000)
+    app.run(threaded=True, host="0.0.0.0", port=5000,debug=True)
